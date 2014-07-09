d8a1ea522210d09725e4dda65517381e2840a8ad
Dan Sackett <danesackett@gmail.com>
2014-07-09 10:33:46 -0400

    Update old build

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,10 @@
 (function() {
+  var app;
 
+  app = angular.module('barebones', []);
+
+  app.controller('mainController', function($scope) {
+    return $scope.appName = 'BareBones';
+  });
 
 }).call(this);
